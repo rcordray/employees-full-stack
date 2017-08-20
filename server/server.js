23e258290ab7b9085826a7d2fde5b1e665a850c1
Robert Cordray <robertcordray@gmail.com>
2017-08-20 01:26:43 -0500

    server is running

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 5000
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, function() {
+    console.log('listening on port', port);
+});
