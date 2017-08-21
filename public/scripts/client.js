8cafa0f2ff233eecf044eb48d6f38ff6f48ce260
Robert Cordray <robertcordray@gmail.com>
2017-08-21 03:47:28 -0500

    css styled

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,33 @@
 console.log('javascript sourced');
+
+var app = angular.module('EmployeeApp', []);
+
+app.controller('Controller', ['$http', function($http) {
+    console.log('controller has been loaded');
+    var self = this;
+
+    self.employees = [];
+
+
+    self.getEmployees = function() {
+        $http({
+            method: 'POST',
+            url: '/employees',
+            data: self.employees
+        }).then(function(response) {
+            console.log(response.data);
+
+        });
+    }
+
+    self.getMessages = function() {
+        $http({
+            method: 'GET',
+            url: '/employees'
+        }).then(function(response) {
+            console.log(response.data);
+            self.message = response.data
+        });
+    }
+    self.getMessages();
+}]);
